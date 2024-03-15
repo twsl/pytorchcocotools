ca1d15b87b3251773e3838cdc6f314e60eecc288
twsl <45483159+twsl@users.noreply.github.com>
2024-03-15 22:18:43 +0000

    Add dict dataclass

--- src/pytorchcocotools/utils.py
+++ src/pytorchcocotools/utils.py
@@ -1,4 +1,14 @@
+from dataclasses import dataclass
 import logging
+
+
+def dataclass_dict(
+    cls=None,
+):
+    cls = dataclass(cls)
+    cls.__getitem__ = lambda self, key: self.__dict__[key]
+    cls.__setitem__ = lambda self, key, value: self.__dict__.update({key: value})
+    return cls
 
 
 def get_logger(name: str) -> logging.Logger:
