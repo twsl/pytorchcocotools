c3c0d9b768f7b03702fb03b19efb5ed2fd420a52
twsl <45483159+twsl@users.noreply.github.com>
2024-10-20 22:34:52 +0000

    Update project

--- src/pytorchcocotools/internal/__init__.py
+++ src/pytorchcocotools/internal/__init__.py
@@ -1,2 +1,4 @@
 # makre sure to detach the tensor and disable the gradient computation
 # https://stackoverflow.com/questions/63582590/why-do-we-call-detach-before-calling-numpy-on-a-pytorch-tensor/63869655#63869655
+
+# dont use torch.tensor, it always copies data
